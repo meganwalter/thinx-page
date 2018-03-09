d7d1fd38782dc48f5456528371bd2259762c7de5
Megan Walter <megan.elizabeth.walter@gmail.com>
2018-03-09 13:57:35 -0500

    adding lightbox

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,4 @@
 var cartQty = 0;
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    center: true,
-    dots:true,
-    dotsEach: true,
-    items: 1
-  });
-});
 
 $('.add-qty').click(function () {
   cartQty ++;
@@ -57,6 +48,29 @@ function setTopOfRelated() {
 }
 
 $(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    center: true,
+    dots:true,
+    dotsEach: true,
+    items: 1
+  });
+
+  $('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
   $('#quantity').text(cartQty);
   $('#quantity-m').text(cartQty);
   setTopOfRelated();
