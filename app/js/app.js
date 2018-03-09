db49aaa67027010e056283c1b01dc02f66c9b7f3
Megan Walter <megwalter@hotmail.com>
2018-03-08 21:03:51 -0500

    updates to the scroll

--- app/js/app.js
+++ app/js/app.js
@@ -7,3 +7,31 @@ $(document).ready(function(){
     items: 1
   });
 });
+
+var container = $('.img-col')[0];
+var posClass = 'under';
+var cols = $('.fixed-cols');
+
+function onScroll() {
+  // Get the viewport props
+  var vh = window.innerHeight;
+  var vy = window.pageYOffset;
+
+  // Get the container props
+  var bounds = container.getBoundingClientRect().height;
+  var ct = bounds.top
+  var cb = bounds.bottom;
+
+  if( (bounds - vy) >= vh) {
+    $.each(cols, function (i, el) {
+      el.classList.remove('absolute-cols')
+    });
+  } else if ( (bounds - vy) < vh) {
+    $.each(cols, function (i, el) {
+      el.classList.add('absolute-cols')
+    });
+  }
+}
+
+window.addEventListener('scroll', onScroll, false);
+onScroll();
